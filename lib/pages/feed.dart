d0d7dd8f45fed58b130482f644d9a2a1f559624e
Joseph Yu <josephxyyu@gmail.com>
2024-07-15 14:02:37 +1000

    Added menu side drawer - add community is now moved to the side drawer - profile page is also added there

--- lib/pages/feed.dart
+++ lib/pages/feed.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:vic_hack_mobile/pages/new_post.dart';
+import 'package:firebase_ui_auth/firebase_ui_auth.dart';
 
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
@@ -18,49 +19,95 @@ class _FeedPageState extends State<FeedPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-                icon: const Icon(Icons.add),
-                onPressed: () => Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => const NewPostPage()),
-                    )),
+      appBar: AppBar(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text(
+                'Simple Flutter App',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 24,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: const Icon(Icons.add),
+              title: const Text('Create Community'),
+              onTap: () {
+                Navigator.of(context).pop();
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const NewPostPage()),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.person),
+              title: const Text('Profile'),
+              onTap: () {
+                Navigator.pop(context);
+
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ProfileScreen(
+                      appBar: AppBar(
+                        leading: IconButton(
+                          icon: const Icon(Icons.arrow_back),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         ),
-        body: SafeArea(
-            child: StreamBuilder<QuerySnapshot>(
-                stream: collectionStream,
-                builder: (BuildContext context,
-                    AsyncSnapshot<QuerySnapshot> snapshot) {
-                  if (snapshot.hasError) {
-                    return const Text('Something went wrong');
-                  }
+      ),
+      body: SafeArea(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: collectionStream,
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('Something went wrong');
+            }
 
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Text("Loading");
-                  }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text("Loading");
+            }
 
-                  if (snapshot.data!.docs.isEmpty) {
-                    return const Center(child: Text("No posts found"));
-                  }
+            if (snapshot.data!.docs.isEmpty) {
+              return const Center(child: Text("No posts found"));
+            }
 
-                  return ListView(
-                    children:
-                        snapshot.data!.docs.map((DocumentSnapshot document) {
-                      Map<String, dynamic> data =
-                          document.data()! as Map<String, dynamic>;
-                      // TODO: Make these list tiles navigate to a Post Page where people can comment etc.
-                      return ListTile(
-                        title: Text(data['title']),
-                        subtitle: Text(data['content']),
-                        trailing: data["imageUrl"] != null
-                            ? Image.network(data["imageUrl"])
-                            : null,
-                      );
-                    }).toList(),
-                  );
-                })));
+            return ListView(
+              children: snapshot.data!.docs.map((DocumentSnapshot document) {
+                Map<String, dynamic> data =
+                    document.data()! as Map<String, dynamic>;
+                // TODO: Make these list tiles navigate to a Post Page where people can comment etc.
+                return ListTile(
+                  title: Text(data['title']),
+                  subtitle: Text(data['content']),
+                  trailing: data["imageUrl"] != null
+                      ? Image.network(data["imageUrl"])
+                      : null,
+                );
+              }).toList(),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
